60bf15f79bf5a4667a55a678205aec333c805bb9
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-08-06 18:54:27 -0400

    Action specification test program

---
+++ src/test/actionspectest.lua
@@ -0,0 +1,23 @@
+
+----------------------------------------------------------------------------
+--  actionspectest.lua - Action specification test script
+--
+--  Created: Fri Aug 06 10:21:04 2010 (at Intel Research, Pittsburgh)
+--  Copyright  2010  Tim Niemueller [www.niemueller.de]
+--
+----------------------------------------------------------------------------
+
+-- Licensed under BSD license
+
+require("roslua")
+require("actionlib.action_spec")
+
+roslua.init_node{master_uri=os.getenv("ROS_MASTER_URI"), node_name="actionspectest"}
+
+print()
+print("Action spec tests")
+
+print()
+local actionspec = actionlib_lua.action_spec.get_actionspec("object_manipulation_msgs/Place")
+actionspec:print()
+
