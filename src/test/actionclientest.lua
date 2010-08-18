371065b21a46a6dcc150af7a16e5041e17c72d65
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-08-18 18:24:12 -0400

    ActionClient: completed initial implementation
    + GoalHandle renamed to ClientGoalHandle
    + Break up single FAILED state, although I still think it would be useful,
      the current way it's closer to original actionlib
    + several utility method for goal handle
    + Bug fixes, fixes, fixes
    + works in test case nicely now

--- src/test/actionclientest.lua
+++ src/test/actionclientest.lua
@@ -23,8 +23,31 @@ local acl = actionlib.action_client("/fibonacci", "actionlib_tutorials/Fibonacci
 acl:wait_for_server()
 --acl:cancel_all_goals()
 
-local goal = acl.actspec.goal_spec:instantiate()
-goal.values.order = 5
-acl:send_goal(goal, function (gh) print("Event", gh.state) end)
+GOAL_ORDER = 30
 
-roslua.run()
+function listener(goal_handle, acl)
+   if goal_handle:state_changed() then
+      printf("State changed: %s -> %s", goal_handle.STATE_TO_STRING[goal_handle.last_state],
+	     goal_handle.STATE_TO_STRING[goal_handle.state])
+      if goal_handle:terminal() then
+	 printf("Terminal state reached, issuing new goal")
+	 local goal = acl.actspec.goal_spec:instantiate()
+	 goal.values.order = GOAL_ORDER
+	 acl:send_goal(goal, listener)
+      end
+   elseif goal_handle:feedback_received() then
+      if #goal_handle.feedback.values.sequence == 15 and math.random() > 0.8 then
+	 printf("Cancelling")
+	 goal_handle:cancel()
+      else
+	 printf("Feedback received: (%d) %s", #goal_handle.feedback.values.sequence,
+		table.concat(goal_handle.feedback.values.sequence, ", "))
+      end
+   end
+end
+
+local goal = acl.actspec.goal_spec:instantiate()
+goal.values.order = GOAL_ORDER
+acl:send_goal(goal, listener)
+
+roslua.run(0.5)
