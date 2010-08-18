b008c2ce6fbcfec19a953e2b1ed49ffe63ca2246
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-08-18 18:29:15 -0400

    init: action server creator and documentation

--- src/actionlib/init.lua
+++ src/actionlib/init.lua
@@ -9,19 +9,39 @@
 
 -- Licensed under BSD license
 
---- Actionlib utilities for Lua
+--- Actionlib implementation for Lua.
 -- This module and its sub-modules provide tools to make use of actionlib from
--- withing Lua.
+-- within Lua.
 -- @copyright Tim Niemueller, Carnegie Mellon University, Intel Research Pittsburgh
 -- @release Released under BSD license
 module("actionlib", package.seeall)
 
 require("actionlib.action_spec")
 require("actionlib.action_client")
+require("actionlib.action_server")
 
 get_actionspec = actionlib.action_spec.get_actionspec
 ActionClient   = actionlib.action_client.ActionClient
+ActionServer   = actionlib.action_server.ActionServer
 
+--- Create a new action client.
+-- This is a convenience method for ActionClient:new(). Since no registration
+-- is performed it is ok to use the constructor directly.
+-- @param name name of the action client
+-- @param type type of the action (i.e. the package-prefixed action file)
 function action_client(name, type)
    return ActionClient:new{name=name, type=type}
+end
+
+--- Create a new action server.
+-- This is a convenience method for ActionServer:new(). Since no registration
+-- is performed it is ok to use the constructor directly.
+-- @param name name of the action server
+-- @param type type of the action (i.e. the package-prefixed action file)
+-- @param goal_cb goal callback
+-- @param spin_cb spin callback
+-- @param cancel_cb cancel callback (optional)
+function action_server(name, type, goal_cb, spin_cb, cancel_cb)
+   return ActionServer:new{name=name, type=type, goal_cb=goal_cb,
+			   spin_cb=spin_cb, cancel_cb=cancel_cb}
 end
